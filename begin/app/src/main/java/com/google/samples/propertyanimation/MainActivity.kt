a8bfa128bfff842743f91518b1c6cfb94777a933
endlessrequiem <austin.j.white.96@gmail.com>
2020-07-15 13:21:52 -0700

    star can now fade

--- begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -116,6 +116,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(fadeButton)
+        animator.start()
+
     }
 
     private fun colorizer() {
