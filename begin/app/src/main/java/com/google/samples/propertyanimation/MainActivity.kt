a9cd31d3887c9d87ee7ba6186be123ae5b62305b
endlessrequiem <austin.j.white.96@gmail.com>
2020-07-15 13:33:17 -0700

    background can now change color

--- begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -20,6 +20,8 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.graphics.Color
+import android.graphics.Color.RED
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -125,6 +127,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun colorizer() {
+        var animator = ObjectAnimator.ofArgb(star.parent,"backgroundColor", Color.BLACK, Color.RED)
+        animator.duration = 500
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(colorizeButton)
+        animator.start()
     }
 
     private fun shower() {
